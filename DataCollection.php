2ac70efc57377e6a1d5fa5db927caf72d8612e17
Voronin Alexandr <info@z-site.ru>
2018-08-14 19:38:38 +0300

    Реализация дата колекции

---
+++ DataCollection.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: voroninaleksandr
+ * Date: 13.08.2018
+ * Time: 22:47
+ */
+
+namespace Gora\DTO;
+
+
+class DataCollection implements DataCollectionInterface
+{
+    private $data;
+
+    /**
+     * DataCollection constructor.
+     * @param $data
+     */
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    function isExists($key)
+    {
+        return isset($this->data[$key]);
+    }
+
+    function getValue($key)
+    {
+        if(!$this->isExists($key)){
+           return null;
+        }
+       return $this->data[$key];
+    }
+}
