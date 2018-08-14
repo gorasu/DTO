159136b7a5eed213a70456d2decfaf184c4f0159
Voronin Alexandr <info@z-site.ru>
2018-08-14 19:42:24 +0300

    Добавил метод создания коллекции в интерфейс

--- DataCollectionInterface.php
+++ DataCollectionInterface.php
@@ -8,11 +8,36 @@
 
 namespace Gora\DTO;
 
+/**
+ * Интерфейс для объектов  хранящие данные для создания объекта типа DTO
+ * Interface DataCollectionInterface
+ * @package Gora\DTO
+ */
 interface DataCollectionInterface
 {
 
+    /**
+     * DataCollectionInterface constructor.
+     * @param $data
+     */
     function __construct($data);
+
+    /**
+     * @param $key
+     * @return mixed
+     */
     function isExists($key);
+
+    /**
+     * @param $key
+     * @return mixed
+     */
     function getValue($key);
+
+    /**
+     * @param $data
+     * @return $this
+     */
+    static function create($data);
 
 }
