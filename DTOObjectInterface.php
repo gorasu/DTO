584f2935d8f48a2ae027ae774c823905554f2d0b
Voronin Alexandr <info@z-site.ru>
2018-08-14 19:35:32 +0300

    Добавил интерфейс для работы с объектами

---
+++ DTOObjectInterface.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: voroninaleksandr
+ * Date: 13.08.2018
+ * Time: 23:14
+ */
+
+namespace Gora\DTO;
+
+
+interface DTOObjectInterface
+{
+    function __construct();
+
+}
