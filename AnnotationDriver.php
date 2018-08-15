a656bc8d930b454f1b03cda7a5b390fba74339c0
Voronin Alexandr <info@z-site.ru>
2018-08-15 19:49:27 +0300

    Изменил драйвер аннотации

--- AnnotationDriver.php
+++ AnnotationDriver.php
@@ -19,36 +19,10 @@ class AnnotationDriver implements MappingDriverInterface
 
     /**
      * @param $className
-     * @param DataCollectionInterface $dataCollection
-     */
-    function createObject($className, DataCollectionInterface $dataCollection)
-    {
-        $properties = [];
-        foreach ($this->getProperties($className) as $property){
-            if($dataCollection->isExists($property->getApiName())){
-                $value = $dataCollection->getValue($property->getApiName());
-                if(class_exists($property->getType())){
-                    //@todo избавиться от объявления DataCollection - может быть сюда принимать массив и уже тут создавать коллекцию
-                    $value = $this->createObject($property->getType(),new DataCollection($property->getValue()));
-                }
-
-                $property->setValue($value);
-
-                $properties[] = $property;
-            }
-        }
-        return (new Mapping($className,$properties))->createInstance();
-
-
-    }
-
-
-    /**
-     * @param $className
-     * @return Property[]
+     * @return PropertyInterface[]
      * @throws \ReflectionException
      */
-    private function getProperties($className){
+    public function getProperties($className){
 
         if(!($propertiesReflection = static::getPropertiesReflectionCache($className))){
             $reflectionClass = new \ReflectionClass($className);
