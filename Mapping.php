9a71b3f6b6fcf0d7955d961896b51f74e09e76d0
Voronin Alexandr <info@z-site.ru>
2018-08-15 20:51:01 +0300

    Привел маппинг в интфрейсу + добавил классы для реализации

--- Mapping.php
+++ Mapping.php
@@ -9,37 +9,57 @@
 namespace Gora\DTO;
 
 
+use ReflectionClass;
+
 class Mapping implements MappingInterface
 {
     private $className;
-    /**
-     * @var PropertyInterface[]
-     */
-    private $properties;
+
 
     private $dataCollection;
+    /**
+     * @var MappingDriverInterface
+     */
+    private $mappingDriver;
+
+    /**
+     * @var PropertyInterface
+     */
+    private $properties;
 
 
     /**
      * MappingInterface constructor.
      * @param $class string
-     * @param $properties PropertyInterface[]
+     * @param $mappingDriver MappingDriverInterface
      */
-    public function __construct($className, $properties)
+    public function __construct($className, MappingDriverInterface $mappingDriver)
     {
-
         $this->className = $className;
-        $this->properties = $properties;
+        $this->mappingDriver = $mappingDriver;
+        $this->setProperties($this->getMappingDriver()->createProperties($className));
+
     }
 
+
     /**
-     *
+     * @return DTOObjectInterface|mixed
+     * @throws \Exception
      */
     function createInstance()
     {
-        //@todo проверку на тип объекта сделать что это DTOIntrface
-        $object = new $this->className();
-        foreach ($this->properties as $property){
+
+
+        $this->fillValuesToProperties();
+
+        $object = $this->createDtoObject();
+
+        foreach ($this->getProperties() as $property) {
+
+            if($property->isRequired() && empty($property->getValue())){
+                throw  new \Exception($property->getName().' обязательно для заполнения');
+            }
+//@Todo заливку приватных методов нужно сделать
             $object->{$property->getName()} = $property->getValue();
         }
         return $object;
@@ -56,4 +76,76 @@ class Mapping implements MappingInterface
         $this->dataCollection = $dataCollection;
         return $this;
     }
+
+    /**
+     * @return DataCollectionInterface
+     */
+    private function getDataCollection()
+    {
+        return $this->dataCollection;
+    }
+
+    /**
+     * @return DTOObjectInterface|mixed
+     */
+    private function createDtoObject(){
+        $className = $this->getClassName();
+        $reflection=new ReflectionClass($className);
+        if(!$reflection->implementsInterface(DTOObjectInterface::class)){
+            throw  new \Exception($this->getClassName() .' должен быть реализован от интерфеса DTOObjectInterface');
+        }
+        return new $className();
+    }
+
+    /**
+     * @return string
+     */
+    private function getClassName()
+    {
+        return $this->className;
+    }
+
+    /**
+     * @return MappingDriverInterface
+     */
+    private function getMappingDriver()
+    {
+        return $this->mappingDriver;
+    }
+
+    /**
+     * @return PropertyInterface[]
+     */
+    private function getProperties()
+    {
+        return $this->properties;
+    }
+
+    /**
+     * @param PropertyInterface[] $properties
+     */
+    private function setProperties($properties)
+    {
+        $this->properties = $properties;
+    }
+
+    private function fillValuesToProperties(){
+        $dataCollection = $this->getDataCollection();
+        foreach ($this->getProperties() as $property) {
+            if ($dataCollection->isExists($property->getApiName())) {
+                $value = $dataCollection->getValue($property->getApiName());
+                if (class_exists($property->getType())) {
+                    $mapping = new Static($property->getType(), $this->getMappingDriver());
+                    $mapping->setDataCollection($dataCollection::create($value));
+                    $value = $mapping->createInstance();
+
+                }
+
+                $property->setValue($value);
+            }
+        }
+    }
+
+
+
 }
