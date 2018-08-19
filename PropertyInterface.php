aab7390aba29f4ad52318d92cd32fa4326272731
Voronin Alexandr <info@z-site.ru>
2018-08-19 13:46:55 +0300

    PropertyInterface phpdock

--- PropertyInterface.php
+++ PropertyInterface.php
@@ -9,13 +9,41 @@
 namespace Gora\DTO;
 
 
+/**
+ * Interface PropertyInterface
+ * @package Gora\DTO
+ */
 interface PropertyInterface
 {
+    /**
+     * @return PropertyTypeInterface
+     */
     function getType();
+
+    /**
+     * @return string
+     */
     function getName();
+
+    /**
+     * @return string
+     */
     function getApiName();
+
+    /**
+     * @return bool
+     */
     function isRequired();
+
+    /**
+     * @param $value
+     * @return mixed
+     */
     function setValue($value);
+
+    /**
+     * @return mixed
+     */
     function getValue();
 
 }
