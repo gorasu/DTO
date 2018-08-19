1fd2f332dc143e4fb0cce919a30df90b800d964d
Voronin Alexandr <info@z-site.ru>
2018-08-19 13:46:22 +0300

    Добавил интерфейс для PropertyTypeInterface

---
+++ PropertyTypeInterface.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: voroninaleksandr
+ * Date: 19.08.2018
+ * Time: 13:38
+ */
+
+namespace Gora\DTO;
+
+
+/**
+ * Interface PropertyTypeInterface
+ * @package Gora\DTO
+ */
+interface PropertyTypeInterface
+{
+    /**
+     * PropertyTypeInterface constructor.
+     * @param $type string
+     */
+    function __construct($type);
+
+    /**
+     * @return bool
+     */
+    function isArray();
+
+    /**
+     * @return bool
+     */
+    function isDtoObject();
+
+}
