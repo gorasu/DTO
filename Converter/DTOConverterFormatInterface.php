93abb433154ea02a28e7f6ed64b55fedfc2fa13d
Voronin Alexandr <info@z-site.ru>
2018-08-23 22:12:47 +0300

    Добавил конвертор формат

---
+++ Converter/DTOConverterFormatInterface.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: voroninaleksandr
+ * Date: 23.08.2018
+ * Time: 21:50
+ */
+
+namespace Gora\DTO\Converter;
+
+
+use Gora\DTO\DTOObjectInterface;
+use Gora\DTO\MappingDriverInterface;
+
+/**
+ * Interface DTOConverterInterface
+ * @package Gora\DTO\Converter
+ */
+interface DTOConverterFormatInterface
+{
+
+    function __construct(DTOObjectInterface $DTOObject,MappingDriverInterface $mappingDriver);
+
+
+    /**
+     * @return array
+     */
+    function toArray();
+
+    /**
+     * @return string
+     */
+    function toJson();
+
+
+}
