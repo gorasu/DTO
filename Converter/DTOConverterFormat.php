33a9a66225e9fbb19523fc1a4fa2eebabf292b8c
Voronin Alexandr <info@z-site.ru>
2018-08-23 23:01:23 +0300

    Добавил конвретацию объекта DTO в массив

---
+++ Converter/DTOConverterFormat.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: voroninaleksandr
+ * Date: 23.08.2018
+ * Time: 22:43
+ */
+
+namespace Gora\DTO\Converter;
+
+
+use Gora\DTO\DTOObjectInterface;
+use Gora\DTO\Exception\NotDoneException;
+use Gora\DTO\Exception\PropertyIsRequiredException;
+use Gora\DTO\MappingDriverInterface;
+use Gora\DTO\PropertyInterface;
+
+class DTOConverterFormat implements DTOConverterFormatInterface
+{
+
+    /**
+     * @var DTOObjectInterface
+     */
+    private $DTOObject;
+    /**
+     * @var MappingDriverInterface
+     */
+    private $mappingDriver;
+
+    public function __construct(DTOObjectInterface $DTOObject, MappingDriverInterface $mappingDriver)
+    {
+        $this->DTOObject = $DTOObject;
+        $this->mappingDriver = $mappingDriver;
+    }
+
+    /**
+     * @return array
+     * @throws PropertyIsRequiredException
+     */
+    function toArray()
+    {
+       $properties =  $this->getMappingDriver()->createProperties(get_class($this->getDTOObject()));
+       $dtoObject = $this->getDTOObject();
+       $result = [];
+       foreach ($properties as $property){
+
+           $value = $dtoObject->{$property->getName()};
+           $this->validate($value,$property);
+
+           $result[$property->getApiName()] =  $value;
+       }
+       return $result;
+
+    }
+
+    /**
+     * @return string
+     */
+    function toJson()
+    {
+        throw new NotDoneException();
+    }
+
+    /**
+     * @return MappingDriverInterface
+     */
+    private function getMappingDriver()
+    {
+        return $this->mappingDriver;
+    }
+
+    /**
+     * @return DTOObjectInterface
+     */
+    private function getDTOObject()
+    {
+        return $this->DTOObject;
+    }
+
+    /**
+     * @param $value
+     * @param PropertyInterface $property
+     * @throws PropertyIsRequiredException
+     */
+    private function validate($value, PropertyInterface $property){
+
+        if(empty($value) && $property->isRequired()){
+            throw new PropertyIsRequiredException($property);
+        }
+
+    }
+}
