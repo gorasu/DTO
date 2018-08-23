3c5d3291955dd11d97808f2be44bdb7d5f0fd725
Voronin Alexandr <info@z-site.ru>
2018-08-23 22:52:17 +0300

    Реализовал конвертор

---
+++ Converter/DTOConverter.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: voroninaleksandr
+ * Date: 23.08.2018
+ * Time: 21:55
+ */
+
+namespace Gora\DTO\Converter;
+
+
+use Gora\DTO\DTOObjectInterface;
+use Gora\DTO\MappingDriverInterface;
+
+class DTOConverter implements DTOConverterInterface
+{
+
+
+    /**
+     * @var MappingDriverInterface
+     */
+    private $mappingDriver;
+
+    public function __construct(MappingDriverInterface $mappingDriver)
+    {
+
+        $this->mappingDriver = $mappingDriver;
+    }
+
+    /**
+     * @param DTOObjectInterface $DTOObject
+     * @return DTOConverterFormatInterface
+     */
+    function convert(DTOObjectInterface $DTOObject)
+    {
+        return new DTOConverterFormat($DTOObject,$this->mappingDriver);
+    }
+}
