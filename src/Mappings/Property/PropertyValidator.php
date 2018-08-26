78ebee9aca5fe4188a10425b7861515dba9f96f2
Voronin Alexandr <info@z-site.ru>
2018-08-26 17:17:19 +0300

    Добавил валидацию типов

--- src/Mappings/Property/PropertyValidator.php
+++ src/Mappings/Property/PropertyValidator.php
@@ -10,6 +10,7 @@ namespace Gora\DTO\Mappings\Property;
 
 
 use Gora\DTO\Exception\PropertyIsRequiredException;
+use Gora\DTO\Exception\PropertyValidateTypeException;
 
 class PropertyValidator implements PropertyValidatorInterface
 {
@@ -23,5 +24,40 @@ class PropertyValidator implements PropertyValidatorInterface
         if(empty($value) && $property->isRequired()) {
             throw new PropertyIsRequiredException($property);
         }
+
+        if(!$this->validateType($value,$property->getType())){
+
+            throw new PropertyValidateTypeException($property);
+        }
+
+
     }
+
+    private function validateType($value, PropertyTypeInterface $propertyType){
+
+        if(empty($value)){
+            return true;
+        }
+        if($propertyType->isArray()){
+            return is_array($value);
+        }
+        if($propertyType->isString()){
+            return is_string($value);
+        }
+        if($propertyType->isBool()){
+            return is_bool($value);
+        }
+        if($propertyType->isInteger()){
+            return is_integer($value);
+        }
+        if($propertyType->isDtoClass() && !$propertyType->isArray()){
+            $className = $propertyType->getDtoClassName();
+            return $value instanceof $className;
+        }
+
+        return true;
+
+    }
+
+
 }
