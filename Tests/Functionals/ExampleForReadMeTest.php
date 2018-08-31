cae2a7f142d3b3e610b528e0bba30233a5ac91e2
Voronin Alexandr <info@z-site.ru>
2018-08-31 07:52:52 +0300

    Тест для того что бы показать редультат работы в Readme

---
+++ Tests/Functionals/ExampleForReadMeTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: voroninaleksandr
+ * Date: 31.08.2018
+ * Time: 7:52
+ */
+
+namespace Gora\DTO\Tests\Functionals;
+
+
+class ExampleForReadMeTest extends \PHPUnit_Framework_TestCase
+{
+
+}
